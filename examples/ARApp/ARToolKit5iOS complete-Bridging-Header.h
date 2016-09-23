089bcba0c3a2573edd8c25313a5934260e4ae8f3
藤澤研学生ユーザ <fujixlab_user1@fujisawakenkyuushitsu1-no-Mac-mini.local>
2016-09-23 15:21:45 +0900

    first commit.

---
+++ examples/ARApp/ARToolKit5iOS complete-Bridging-Header.h
@@ -0,0 +1,18 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+#import "CameraVideo.h"
+#import "ar.h"
+#import "video.h"
+#import "videoiPhone.h"
+#import "gsub_es.h"
+#import "EAGLView.h"
+#import "ARView.h"
+#import "param.h"
+#import "PrefixHeader.pch"
+
+#endif /* Bridging_Header_h */
---
+++ examples/ARApp/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ARToolKit5iOS
+//
+//  Created by 藤澤研究室 on 2016/09/14.
+//
+//
+
+import Foundation
+import UIKit
+
+exit(UIApplicationMain(Process.argc, Process.unsafeArgv, nil, nil))
